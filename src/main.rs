775058f5ed39023493d8d83ab10420d87632ac56
Abiria <abiria.dev@gmail.com>
2023-04-20 02:35:40 +0900

    feat: use clap for cmdline parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use clap::Parser;
 use time::{OffsetDateTime, Time};
 
+#[derive(Parser)]
+#[command(version, about)]
+struct Opts;
+
 fn main() {
+	let _opts = Opts::parse();
+
 	let now = OffsetDateTime::now_local().unwrap();
 	let now = now.replace_time(Time::MIDNIGHT);
 	println!("{}", now.unix_timestamp());
