7c73d57de81136560eb57348c9f188ae6bfbefe0
Abiria <abiria.dev@gmail.com>
2023-04-20 03:08:19 +0900

    feat: option to use discord format

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ use time::{OffsetDateTime, Time};
 
 #[derive(Parser)]
 #[command(version, about)]
-struct Opts;
+struct Opts {
+	/// Use Discord's timestamp format.
+	#[arg(short, long)]
+	discord: bool,
+}
 
 fn main() {
-	let _opts = Opts::parse();
+	let opts = Opts::parse();
 
 	let now = OffsetDateTime::now_local().unwrap();
 	let now = now.replace_time(Time::MIDNIGHT);
-	println!("{}", now.unix_timestamp());
+
+	let ts_string = format(now.unix_timestamp(), &opts);
+	println!("{ts_string}");
+}
+
+fn format(ts: i64, opts: &Opts) -> String {
+	if opts.discord {
+		format!("<t:{ts}:D>")
+	} else {
+		ts.to_string()
+	}
 }
