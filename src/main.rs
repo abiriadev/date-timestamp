9c5d5c6642618e843b161a7de9c666785a017280
Abiria <abiria.dev@gmail.com>
2023-04-20 03:13:18 +0900

    feat: option to use UTC time

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ struct Opts {
 	/// Use Discord's timestamp format.
 	#[arg(short, long)]
 	discord: bool,
+
+	/// Use UTC time instead of local time.
+	#[arg(short, long)]
+	utc: bool,
 }
 
 fn main() {
 	let opts = Opts::parse();
 
-	let now = OffsetDateTime::now_local().unwrap();
+	let now = current(&opts);
 	let now = now.replace_time(Time::MIDNIGHT);
 
 	let ts_string = format(now.unix_timestamp(), &opts);
@@ -24,5 +28,13 @@ fn format(ts: i64, opts: &Opts) -> String {
 		format!("<t:{ts}:D>")
 	} else {
 		ts.to_string()
+	}
+}
+
+fn current(opts: &Opts) -> OffsetDateTime {
+	if opts.utc {
+		OffsetDateTime::now_utc()
+	} else {
+		OffsetDateTime::now_local().unwrap()
 	}
 }
