336b11926e853b39e4a95de59ee3f459f7013d1e
Abiria <abiria.dev@gmail.com>
2023-04-20 03:32:15 +0900

    feat: option to print human-readable date

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use time::{OffsetDateTime, Time};
+use time::{format_description::well_known::Rfc2822, OffsetDateTime, Time};
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -11,6 +11,10 @@ struct Opts {
 	/// Use UTC time instead of local time.
 	#[arg(short, long)]
 	utc: bool,
+
+	/// Print human readable date along with timestamp.
+	#[arg(short = 'p', long)]
+	date: bool,
 }
 
 fn main() {
@@ -19,7 +23,12 @@ fn main() {
 	let now = current(&opts);
 	let now = now.replace_time(Time::MIDNIGHT);
 
-	let ts_string = format(now.unix_timestamp(), &opts);
+	let ts_string = print_date(
+		format(now.unix_timestamp(), &opts),
+		now,
+		&opts,
+	);
+
 	println!("{ts_string}");
 }
 
@@ -36,5 +45,16 @@ fn current(opts: &Opts) -> OffsetDateTime {
 		OffsetDateTime::now_utc()
 	} else {
 		OffsetDateTime::now_local().unwrap()
+	}
+}
+
+fn print_date(text: String, time: OffsetDateTime, opts: &Opts) -> String {
+	if opts.date {
+		format!(
+			"{}\n{text}",
+			time.format(&Rfc2822).unwrap()
+		)
+	} else {
+		text
 	}
 }
